cf5a575dbc21d33a7fb5caec25e86f98f1c80ae1
Jesus Zuñiga <jesuszt@hotmail.com>
2022-01-13 11:20:06 -0600

    Modificaciones 13/01/2022

--- src/main/java/org/zutjmx/apiservlet/webapp/form/FormServlet.java
+++ src/main/java/org/zutjmx/apiservlet/webapp/form/FormServlet.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -17,6 +18,13 @@ public class FormServlet extends HttpServlet {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
+        String pais = req.getParameter("pais");
+        String idioma = req.getParameter("idioma");
+        String habilitar = req.getParameter("habilitar");
+        String secreto = req.getParameter("secreto");
+        String[] lenguajes = req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
+
         resp.setContentType("text/html");
 
         try(PrintWriter out = resp.getWriter()) {
@@ -32,6 +40,24 @@ public class FormServlet extends HttpServlet {
             out.println("           <li>Usuario: " + username + "</li>");
             out.println("           <li>Contraseña: " + password + "</li>");
             out.println("           <li>Correo electrónico: " + email + "</li>");
+            out.println("           <li>País: " + pais + "</li>");
+
+            out.println("           <li>Lenguajes: <ul>");
+            Arrays.asList(lenguajes).forEach(lenguaje -> {
+                out.println("           <li>" + lenguaje + "</li>");
+            });
+            out.println("           </ul></li>");
+
+            out.println("           <li>Roles: <ul>");
+            Arrays.asList(roles).forEach(role -> {
+                out.println("           <li>" + role + "</li>");
+            });
+            out.println("           </ul></li>");
+
+            out.println("           <li>Idioma: " + idioma + "</li>");
+            out.println("           <li>Habilitado: " + habilitar + "</li>");
+            out.println("           <li>Secreto: " + secreto + "</li>");
+
             out.println("      </ul>");
             out.println("  </body>");
             out.println("</html>");

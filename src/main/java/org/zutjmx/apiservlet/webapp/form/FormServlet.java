f1f42cc7593110ee09d917fe78be1d66afae94bf
Jesus Zuñiga <jesuszt@hotmail.com>
2022-01-17 11:11:04 -0600

    Modificaciones 17/01/2021

--- src/main/java/org/zutjmx/apiservlet/webapp/form/FormServlet.java
+++ src/main/java/org/zutjmx/apiservlet/webapp/form/FormServlet.java
@@ -8,9 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
@@ -26,50 +24,52 @@ public class FormServlet extends HttpServlet {
         String secreto = req.getParameter("secreto");
         String[] lenguajes = req.getParameterValues("lenguajes");
         String[] roles = req.getParameterValues("roles");
-        List<String> errores = new ArrayList<>();
+        //List<String> errores = new ArrayList<>();
+        Map<String, String> errores = new HashMap<>();
 
         if (username == null || username.isBlank()) {
-            errores.add("El usuario es requerido");
+            errores.put("username","El usuario es requerido");
         }
 
         if (password == null || password.isBlank()) {
-            errores.add("La contraseña es requerida");
+            errores.put("password","La contraseña es requerida");
         }
 
         if (email == null || !email.contains("@")) {
-            errores.add("El correo electrónico es requerido y debe de ser válido");
+            errores.put("email","El correo electrónico es requerido y debe de ser válido");
         }
 
         if (pais == null || pais.isBlank()) {
-            errores.add("El pais es requerido");
+            errores.put("pais","El pais es requerido");
         }
 
         if (lenguajes == null || lenguajes.length == 0) {
-            errores.add("Al menos un lenguaje es requerido");
+            errores.put("lenguajes","Al menos un lenguaje es requerido");
         }
 
         if (roles == null || roles.length == 0) {
-            errores.add("Al menos un rol es requerido");
+            errores.put("roles","Al menos un rol es requerido");
         }
 
         if (idioma == null) {
-            errores.add("El idioma es requerido");
+            errores.put("idioma","El idioma es requerido");
         }
 
         resp.setContentType("text/html");
 
-        try(PrintWriter out = resp.getWriter()) {
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("    <head>");
-            out.println("      <meta charset=\"UTF-8\">");
-            out.println("      <title>Resultado de los datos enviados en el formulario</title>");
-            out.println("    </head>");
-            out.println("  <body>");
-            out.println("      <h1>Resultado de los datos enviados en el formulario</h1>");
-            out.println("      <ul>");
+        if (errores.isEmpty()) {
+            try (PrintWriter out = resp.getWriter()) {
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("    <head>");
+                out.println("      <meta charset=\"UTF-8\">");
+                out.println("      <title>Resultado de los datos enviados en el formulario</title>");
+                out.println("    </head>");
+                out.println("  <body>");
+                out.println("      <h1>Resultado de los datos enviados en el formulario</h1>");
+                out.println("      <ul>");
 
-            if (errores.isEmpty()) {
+
                 out.println("           <li>Usuario: " + username + "</li>");
                 out.println("           <li>Contraseña: " + password + "</li>");
                 out.println("           <li>Correo electrónico: " + email + "</li>");
@@ -90,15 +90,18 @@ public class FormServlet extends HttpServlet {
                 out.println("           <li>Idioma: " + idioma + "</li>");
                 out.println("           <li>Habilitado: " + habilitar + "</li>");
                 out.println("           <li>Secreto: " + secreto + "</li>");
-            } else {
-                errores.forEach(error -> {
+
+                out.println("      </ul>");
+                out.println("  </body>");
+                out.println("</html>");
+            }
+        } else {
+                /*errores.forEach(error -> {
                     out.println("<li>" + error + "</li>");
                 });
-                out.println("<p><a href=\"/webapp-form/index.html\">volver</a></p>");
-            }
-            out.println("      </ul>");
-            out.println("  </body>");
-            out.println("</html>");
+                out.println("<p><a href=\"/webapp-form/index.jsp\">volver</a></p>");*/
+            req.setAttribute("errores", errores);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
         }
 
     }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,8 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="java.util.Map" %>
+<%
+    Map<String, String> errores = (Map<String, String>)request.getAttribute("errores");
+%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,24 +11,50 @@
 </head>
 <body>
     <h3>Formulario de Usuarios</h3>
+    <%
+        if(errores != null && errores.size()>0) {
+    %>
+    <ul>
+        <% for(String error: errores.values()) {%>
+        <li><%=error%></li>
+        <%}%>
+    </ul>
+    <%
+        }
+    %>
     <form action="/webapp-form/registro" method="post">
         <div>
             <label for="username">Usuario</label>
             <div>
                 <input type="text" name="username" id="username">
             </div>
+            <%
+                if(errores != null && errores.containsKey("username")) {
+                    out.println("<small style='color: red;'>" + errores.get("username") + "</small>");
+                }
+            %>
         </div>
         <div>
             <label for="password">Contraseña</label>
             <div>
                 <input type="password" name="password" id="password">
             </div>
+            <%
+                if(errores != null && errores.containsKey("password")) {
+                    out.println("<small style='color: red;'>" + errores.get("password") + "</small>");
+                }
+            %>
         </div>
         <div>
             <label for="email">Correo Electrónico</label>
             <div>
                 <input type="text" name="email" id="email">
             </div>
+            <%
+                if(errores != null && errores.containsKey("email")) {
+                    out.println("<small style='color: red;'>" + errores.get("email") + "</small>");
+                }
+            %>
         </div>
         <div>
             <label for="pais">País</label>
@@ -39,6 +70,11 @@
                     <option value="MX" selected>México</option>
                 </select>
             </div>
+            <%
+                if(errores != null && errores.containsKey("pais")) {
+                    out.println("<small style='color: red;'>" + errores.get("pais") + "</small>");
+                }
+            %>
         </div>
         <div>
             <label for="lenguajes">Lenguajes de programación</label>
@@ -56,6 +92,11 @@
                     <option value="springboot">SpringBoot</option>
                 </select>
             </div>
+            <%
+                if(errores != null && errores.containsKey("lenguajes")) {
+                    out.println("<small style='color: red;'>" + errores.get("lenguajes") + "</small>");
+                }
+            %>
         </div>
         <div>
             <label>Roles</label>
@@ -78,6 +119,11 @@
                 <label>Moderador</label>
             </div>
         </div>
+        <%
+            if(errores != null && errores.containsKey("roles")) {
+                out.println("<small style='color: red;'>" + errores.get("roles") + "</small>");
+            }
+        %>
         <div>
             <label>Idiomas: </label>
             <div>
@@ -104,6 +150,11 @@
                 <input type="radio" name="idioma" value="ch">
                 <label>Chino</label>
             </div>
+            <%
+                if(errores != null && errores.containsKey("idioma")) {
+                    out.println("<small style='color: red;'>" + errores.get("idioma") + "</small>");
+                }
+            %>
         </div>
         <div>
             <label for="habilitar">Habilitar</label>
